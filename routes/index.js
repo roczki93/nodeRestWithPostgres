534458a4bbb7ae0ab69a0a493d18ef162d89cf5d
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-10 15:24:24 +0200

    post to auth user by username and password add token gen

--- routes/index.js
+++ routes/index.js
@@ -54,6 +54,7 @@ router.post('/api/v1/auth', (req, res, next) => {
     [data.username, data.pass]);
     var tmp=false;
 	var token=null;
+	query_add_token;
 	query.on('row', (row) => {
       console.log(row.id);
 	  if(''!=row.id){
