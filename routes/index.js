cb04365bba51843915ec040ceadc9025346f0117
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-10 11:56:58 +0200

    add new user

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,36 @@ const pg = require('pg');
 const path = require('path');
 const connectionString = process.env.DATABASE_URL || 'postgres://myuser:mypass@192.168.177.183:5432/rest';
 
+//add user
+router.post('/api/v1/auth/add', (req, res, next) => {
+  const results = [];
+  // Grab data from http request
+  const data = {username: req.body.username, pass: false};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    [data.username, data.pass]);
+    client.query('INSERT INTO users(username, pass) values($1, $2)',
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM items ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+//end-add user
 router.post('/api/v1/todos', (req, res, next) => {
   const results = [];
   // Grab data from http request
